4f6adb61b0aceea4d5dc0c1b53133d459a2bf42a
Dhirenvinay <mailtoodhiren@gmail.com>
2025-01-20 13:32:41 +0530

    latest version

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => {
